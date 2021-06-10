dc1ab7ccc0795f33d4ce8fdeb4e8d1789c46a571
yacicastro <aldanayaci@gmail.com>
2021-06-10 19:28:06 -0300

    Desafio3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/CartWidget/ItemListContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Escribir componentes: function y clase
+// componentes clases definidos mediante clase
+//poseen metodo importante llamado render
+//extends esta relacionado a herencia, la calse app hereda de  react.component
+//los componentews tipo clase se usaban para guardad un estado
+//poseeen ciclos de vida d elos componentes
+//state y lifecycle metodos no lo poseian componentes de tipo function
+//antes functiom solo devolvia jsxn se los denominaba dumby
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <ItemListContainer/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import CartWidget from "../CartWidget/CartWidget"
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className="nav">
+                <script src="https://kit.fontawesome.com/7e5b2d153f.js" crossorigin="anonymous"></script>
+                <div className="nav-div">
+                    <a className="logo nav-link">Logo</a>
+                    <button class="nav-toggle" aria-label="Abrir menú">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <ul className="nav-menu nav-menu_visible">
+                        <li className="nav-menu-item">
+                            <a className="nav-menu-link nav-link">Servicios</a>
+                        </li>
+                        <li className="nav-menu-item">
+                            <a className="nav-menu-link nav-link">Diseños</a>
+                        </li>
+                        <li className="nav-menu-item">
+                            <a className="nav-menu-link nav-link">Galeria</a>
+                        </li>
+                        <li className="nav-menu-item">
+                            <a className="nav-menu-link nav-link">Contacto</a>
+                        </li>
+                        <li className="nav-menu-item">
+                            <a><CartWidget/></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+export default NavBar;
